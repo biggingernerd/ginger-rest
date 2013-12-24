c466e5fdac8a2293fa375e99103c6b6f2544e8a5
Martijn van Maasakkers <martijn@vanmaasakkers.net>
2013-12-24 16:58:34 +0100

    Added docblocks

--- src/Ginger/Options.php
+++ src/Ginger/Options.php
@@ -1,9 +1,20 @@
 <?php
-
+/**
+ * Options class.
+ */
 namespace Ginger;
 
+/**
+ * Options class.
+ */
 class Options extends \Ginger\Singleton
 {
+    /**
+     * locale
+     * 
+     * @var mixed
+     * @access public
+     */
     public $locale;
     
 }
